6405b2b65a12581711bbece681c700a2c8114f5f
Samuel Ytterbrink <neppord@gmail.com>
2022-02-13 15:34:06 +0100

    If the current path is not a directory show help message

--- src/Main.purs
+++ src/Main.purs
@@ -3,17 +3,18 @@ module Main where
 import Prelude
 
 import Data.Array (foldMap)
+import Data.Maybe (fromMaybe)
 import Data.String (stripSuffix)
+import Data.String.Pattern (Pattern(..))
 import Effect (Effect)
 import Effect.Aff.Class (liftAff)
 import Effect.Console (log)
+import Html (a, li, ul)
 import Node.Express.App (AppM, get, listenHttp)
 import Node.Express.Request (getPath)
 import Node.Express.Response (send, setStatus)
-import Node.FS.Aff (readdir)
-import Data.String.Pattern (Pattern(..))
-import Data.Maybe (fromMaybe)
-import Html (a, li, ul)
+import Node.FS.Aff (readdir, stat)
+import Node.FS.Stats (isDirectory)
 
 renderFileTree :: String -> Array String -> String
 renderFileTree path files = files
@@ -24,11 +25,15 @@ app :: AppM Unit
 app = do
     get "*" $ do
         path <- getPath
+        let filePath = "." <> path
         setStatus 200
-        dir <- liftAff $ readdir ("." <> path)
-        let linkPrefix = stripSuffix (Pattern "/") path
-                # fromMaybe path
-        send $ renderFileTree linkPrefix dir
-
+        s <- liftAff $ stat filePath
+        if isDirectory s then do
+            dir <- liftAff $ readdir filePath
+            let linkPrefix = stripSuffix (Pattern "/") path
+                    # fromMaybe path
+            send $ renderFileTree linkPrefix dir
+        else
+            send "Not a directory"
 main :: Effect Unit
 main = void $ listenHttp app 8080 (\_ -> log "listening to port 8080")
